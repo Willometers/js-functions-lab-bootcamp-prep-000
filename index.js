1bb3673e59186cf1cdc07b18869a7045dfa1c499
Bill Miles <williammiles201@gmail.com>
2020-10-14 12:51:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,20 +6,20 @@ function happyHolidays() {
   return "Happy holidays!"
 }
 function happyHolidaysTo() {
-  return 'Happy holidays ${name}!'
+  return `Happy holidays ${name}!`
 }
 function happyHolidaysTo() {
-  return 'Happy holidays ${name}!'
+  return `Happy holidays ${name}!`
 }
 function happyCustomHolidayTo() {
-  return 'Happy ${holiday} ${name}!'
+  return `Happy ${holiday} ${name}!`
 }
 function happyCustomHolidayTo() {
-  return 'Happy ${holiday} ${name}!'
+  return `Happy ${holiday} ${name}!`
 }
 function holidayCountdown() {
-  return 'It\'s ${days} days until ${holiday}!'
+  return `It\'s ${days} days until ${holiday}!`
 }
 function holidayCountdown() {
-  return 'It\'s ${days} days until ${holiday}!'
+  return `It\'s ${days} days until ${holiday}!`
 }
