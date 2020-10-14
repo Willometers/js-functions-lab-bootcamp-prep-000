d042ec5f6392d8542027eaf9456e2c080b444b94
Bill Miles <williammiles201@gmail.com>
2020-10-14 13:12:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ var holiday = "Independence Day", "New Year", "Mother's Day", "Father's Day"
 var days = 20, 120
 
 function happyHolidays() {
-  return(`Happy holidays!`)
+  return`Happy holidays!`
 }
 function happyHolidaysTo(you) {
  var name = "you"
- return(`Happy holidays ${name}!`)
+ return`Happy holidays ${name}!`
 }
 function happyHolidaysTo(Independence Day, you) {
   return`Happy ${holiday} ${name}!`
