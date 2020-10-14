4b2daa5fd0fd8ce607e222c0eae927e1e8f25a9f
Bill Miles <williammiles201@gmail.com>
2020-10-14 12:21:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// write your code below!
+var name = 'to, you'
+var holiday = 'Mother\'s Day!'
+var days = '20'
+
+function happyHolidays() {
+  return 'Happy holidays!'
+}
+function happyHolidaysTo() {
+  return 'Happy holidays ${name}!'
+}
+function happyCustomHolidayTo() {
+  return 'Happy ${holiday} ${name}!'
+}
+function holidayCountdown() {
+  return 'It\'s ${days} days until ${holiday}!'
+}
