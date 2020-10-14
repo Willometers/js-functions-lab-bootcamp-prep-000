e5d574638bbbeddb2439bea3ac1b507fb38fdc6e
Bill Miles <williammiles201@gmail.com>
2020-10-14 13:33:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function happyHolidaysTo('you') {
 function happyHolidaysTo(Independence Day, 'you') {
   return `Happy ${holiday}, ${name}!`
 }
-function holidayCountdown(20, Mother\'s Day) {
+function holidayCountdown(20, "Mother's Day") {
   return `It's ${days} days until ${holiday}!`
 }
 function holidayCountdown(120, Father\'s Day) {
