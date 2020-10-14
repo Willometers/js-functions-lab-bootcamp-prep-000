7837cdbfe8aa88f42ae95af7b089758c558894a6
Bill Miles <williammiles201@gmail.com>
2020-10-14 12:37:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var name = "you"
 function happyHolidays() {
   return 'Happy holidays!'
 }
-function happyHolidaysTo() {
+function happyHolidaysTo('you') {
   return 'Happy holidays ${name}!'
 }
 function happyCustomHolidayTo() {
